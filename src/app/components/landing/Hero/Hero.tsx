e958c08d28c70b8c39194fbdfbce6bcfa32cc807
Bibiwei <bibiweipere@gmail.com>
2024-07-09 01:51:04 +0100

    Landing page

--- src/app/components/landing/Hero/Hero.tsx
+++ src/app/components/landing/Hero/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
 				<div className="img"></div>
 				<div className="hero">
 					<h1>Livestream The Landmark Chess Week</h1>
-					<p>Can't make it in person? No problem! Stream the event LIVE exclusively on the Oyoyo Events App!</p>
+					<p>Can&apos;t make it in person? No problem! Stream the event LIVE exclusively on the Oyoyo Events App!</p>
 					<Button className="max-w-[142px]">Get early access</Button>
 					<div className="video">
 						<Image src={Add} alt="Add" />
